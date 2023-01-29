0b2f598894b662c79f69d0285fe3c93360996b0a
jasmine-aonach <jasmine@aonach.com>
2023-01-29 16:57:22 +0000

    finished with globack changes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>this is test</h1>
     </div>
   );
 }
