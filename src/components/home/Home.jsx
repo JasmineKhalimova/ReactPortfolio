e23fcbfdd481c7c99ec3da3c365f8522d658d3dc
jasmine-aonach <jasmine@aonach.com>
2023-02-06 13:36:32 +0000

    About Section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -13,13 +13,13 @@ const Home = () => {
                     <h1 className='home__title'>Jasmine Khalimova</h1>
                     <h3 className='home__subtitle'>— Web Developer —</h3>
                     <p className='home__description'>Experienced web developer with a passion for developing innovative applications.</p>
-                    <a href='#contact' className='button button--flex'>Get In Touch <i class="uil uil-message"></i></a>
+                    <a href='#contact' className='button button--flex'>Get In Touch <i className="uil uil-message"></i></a>
                 </div>
             </div>
             <div className='home__scroll'>
                 <a href='#about' className='home__scoll-button button--flex'>
                     <span className='home__scroll-label'> Scroll Down</span>
-                    <i class="uil uil-arrow-down section__title-large home__scroll-arrow"></i>
+                    <i className="uil uil-arrow-down section__title-large home__scroll-arrow"></i>
                 </a>
             </div>
         </div>
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Social = () => {
   return (
     <div className='home__social'>
-        <a href='https://github.com/JasmineKhalimova' className='home__social-icon' target='_blank'>
-            <i class="uil uil-github-alt"></i>
+        <a href='https://github.com/JasmineKhalimova' className='home__social-icon' target='_blank'  rel="noreferrer">
+            <i className="uil uil-github-alt"></i>
         </a>
-        <a href='https://www.linkedin.com/in/jasmine-k-5bb6a9151/' className='home__social-icon' target='_blank'>
-            <i class="uil uil-linkedin-alt"></i>
+        <a href='https://www.linkedin.com/in/jasmine-k-5bb6a9151/' className='home__social-icon' target='_blank'  rel="noreferrer">
+            <i className="uil uil-linkedin-alt"></i>
         </a>
     </div>
   )
